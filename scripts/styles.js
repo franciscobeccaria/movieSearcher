d23377df66315d0e67ff1cc981261e1b3e53cf27
franciscobeccaria <fbeccaria24@gmail.com>
2020-11-29 17:42:40 -0300

    Commit. My Lists done. Responsive done. JS done. JS problem with id not found done.

--- scripts/styles.js
+++ scripts/styles.js
@@ -10,13 +10,33 @@ document.getElementById('close-icon').addEventListener('click', () => {
 });
 
 // Show Modal Custom List
-document.getElementById('btn-custom-list').addEventListener('click', () => {
-  document.getElementById('modal-custom-list').classList.add('show-flex');
-});
+if (document.getElementById('btn-custom-list')) {
+  document.getElementById('btn-custom-list').addEventListener('click', () => {
+    document.getElementById('modal-custom-list').classList.add('show-flex');
+  });
 
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('modal-container')) {
-    document.getElementById('modal-custom-list').classList.remove('show-flex');
-    document.getElementById('modal-searcher-input').value = '';
-  }
-});
+  document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('modal-container')) {
+      document
+        .getElementById('modal-custom-list')
+        .classList.remove('show-flex');
+      document.getElementById('modal-searcher-input').value = '';
+    }
+  });
+}
+
+// Show Modal Create List
+if (document.getElementById('btn-create-list')) {
+  document.getElementById('btn-create-list').addEventListener('click', () => {
+    document.getElementById('modal-custom-list').classList.add('show-flex');
+  });
+
+  document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('modal-container')) {
+      document
+        .getElementById('modal-custom-list')
+        .classList.remove('show-flex');
+      document.getElementById('modal-input').value = '';
+    }
+  });
+}
